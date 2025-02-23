cbe3a1584773d9b1f18e75573311398d5e2c0b1a
Aarshi Garg <aarshi0808.becse24@chitkara.edu.in>
2025-02-23 17:28:14 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -2,19 +2,17 @@
 int main()
 {
     unsigned int a;
-    printf("Enter an integer: ");
     scanf("%u", &a);
 
     unsigned int b = num & (1 << (sizeof(unsigned int) * 8 - 1));
 
     if (b)
     {
-        printf("The Most Significant Bit of %u is 1\n", num);
+        printf("Set");
     }
     else
     {
-        printf("The Most Significant Bit of %u is 0\n", num);
+        printf("Not Set");
     }
-
     return 0;
 }
